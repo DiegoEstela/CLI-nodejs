f9cc8f1c138e2242764e1f524d01d3e533c85015
DiegoEstela <die.estela@gmail.com>
2022-07-22 15:43:06 -0300

    first test

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
+import inquirer from "inquirer";
+import chalk from "chalk";
+import figlet from "figlet";
+import clear from 'clear'
+import fs from 'fs'
+
+clear();
+
+console.log(
+    chalk.yellow(
+      figlet.textSync('Uma Mf', { horizontalLayout: 'full' })
+    )
+  );
+  
+  
+inquirer.prompt([{
+    name: 'Mf',
+    message: 'Nombre del Microfront',
+    default: "mf-name"
+
+},{
+    name: 'texto',
+    message: 'que queres escribir adentro',
+    default: "prueba"
+}
+]).then(file => {
+    console.log(file)
+  fs.writeFile(`${file.Mf}.js`, `${file.texto}`, (err)=> {if(err){ console.log(err)}}) 
+})
